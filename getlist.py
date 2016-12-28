b07438eceefbe0a9fb623ef45b96e1a3bfbe5068
Ubuntu <ubuntu@ip-172-31-2-108.us-west-1.compute.internal>
2016-12-28 20:40:09 +0000

    rev from server

--- getlist.py
+++ getlist.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env /Users/philliptsan/Projects/phun-company/bin/python
+#!/usr/bin/env python
 
 import requests
 import os
@@ -6,13 +6,10 @@ from bs4 import BeautifulSoup
 
 url = "http://www.funko-shop.com/sitemap_products_1.xml"
 
-products = requests.get(url)
+soup = BeautifulSoup(requests.get(url).content,"lxml")
 
-soup = BeautifulSoup(products.content,"lxml")
-
-new = soup.find_all("loc")
+products = soup.find_all("loc")
 file = open("old.txt", "w")
-for product in soup.find_all("loc"):   
-    file.write(product.text + "\n")
-    
+for item in products:
+    file.write(item.text + "\n")
 file.close()
