e12e82016be6a57aff92fec7c653dabd1c0fd501
PUNTxR <philliptsan@gmail.com>
2016-12-30 20:25:37 -0800

    update

--- get_all.py
+++ get_all.py
@@ -9,15 +9,19 @@ os.chdir(os.path.dirname(os.path.abspath(sys.argv[0])))
 site_list = open("sites.txt", "r").read().split("\n")
 
 for site in site_list:
-    soup = BeautifulSoup(requests.get(site).content,"lxml")
-    products = soup.find_all("loc")
-    if "{0.netloc}".format(urlsplit(site))[:-4].startswith('www.'):
-        domain = "{0.netloc}".format(urlsplit(site))[4:-4]
-    else:
-        domain = "{0.netloc}".format(urlsplit(site))[:-4]
-    file = open(os.getcwd() + "/files/" + domain + "-new.txt", "w")
-    for item in products:
-        file.write(item.text + "\n")
-    file.close()
+    if site is not []:
+        print(site)
+        soup = BeautifulSoup(requests.get(site).content,"lxml")
+        #print(soup)
+        products = soup.find_all("loc")
+        if "{0.netloc}".format(urlsplit(site))[:-4].startswith('www.'):
+            domain = "{0.netloc}".format(urlsplit(site))[4:-4]
+        else:
+            domain = "{0.netloc}".format(urlsplit(site))[:-4]
+        file = open(os.getcwd() + "/files/" + domain + "-new.txt", "w")
+        for item in products:
+            file.write(item.text + "\n")
+        file.close()
 
 exec(open("./read_all.py").read())
+
